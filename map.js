f65cf6324faa8c267fa0ede2757dff2c12d10b3e
Aaron Lu <aaronnoh0409@gmail.com>
2022-07-14 14:17:20 -0400

    fixed displaying older group of location when clicked new set of location, added remove button

--- map.js
+++ map.js
@@ -7,18 +7,19 @@ function showPosition(position) {
     let long = position.coords.longitude;
 
     // building map
-    const map = L.map('map', {
+    const leafMap = L.map('map', {
         center: [lat, long],
         zoom: 15,
     })
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map)
+    }).addTo(leafMap)
     // display marker by the user's cord
-    L.marker([lat, long]).addTo(map).bindPopup("My location")
+    L.marker([lat, long]).addTo(leafMap).bindPopup("My location")
     // shows the proximity of 2500 of users location if the user wants
-    L.control.locate().addTo(map)
+    L.control.locate().addTo(leafMap)
 
+    const layerGroup = L.layerGroup().addTo(leafMap);
 
     async function getFoursquare(business) {
         let clientId = '3J5YYNCNKZRXEAIRVG3SBTUIGGHSSZLSUYVGAL4IPG0EPA34'
@@ -28,19 +29,26 @@ function showPosition(position) {
             `https://api.foursquare.com/v2/venues/explore?client_id=${clientId}&client_secret=${clentSecret}&v=20180323&limit=${limit}&ll=${lat},${long}&query=${business}`
         )
         let data = await response.json()
+        console.log(data)
         let items = data.response.groups[0].items
 
         for (let index = 0; index < limit; index++) {
             console.log(items[index])
-            L.marker([items[index].venue.location.lat, items[index].venue.location.lng]).addTo(map).bindPopup(`${items[index].venue.name}`)
+            L.marker([items[index].venue.location.lat, items[index].venue.location.lng]).addTo(layerGroup).bindPopup(`${items[index].venue.name}`)
 
         }
     }
 
     document.getElementById('find').addEventListener('click', () => {
         let business = document.getElementById("selections").value
+
+        layerGroup.clearLayers();
+
         getFoursquare(business)
     })
+    
+    document.getElementById("clearSelection").addEventListener("click", () =>
+        layerGroup.clearLayers());
 }
 
 // create map
